44182bd5668aab1ea24b640b49a9eedc8d5e2d62
aman takey <amantakey008@gmail.com>
2020-02-22 21:47:40 +0530

    added sort algorithms

---
+++ core/Algorithms/Searching and Sorting/Sorting/BubbleSort.cs
@@ -0,0 +1,37 @@
+﻿using Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Algorithms.Searching_and_Sorting.Sorting
+{
+    public class BubbleSort : IPracticeExecutor
+    {
+        public void Execute()
+        {
+            List<int> list = new List<int>() { 1, 5, 6, 4, 6,10,9, 8, 5 };
+            PrintSortedList(list);
+        }
+
+        public void PrintSortedList(List<int> list)
+        {
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                for (int j = 0; j < list.Count - i - 1; j++)
+                {
+                    if (list[j] > list[j + 1])
+                    {
+                        int temp = list[j];
+                        list[j] = list[j + 1];
+                        list[j + 1] = temp;
+                    }
+                }
+            }
+
+            list.ForEach(_ =>
+            {
+                Console.WriteLine(_);
+            });
+        }
+    }
+}
---
+++ core/Algorithms/Searching and Sorting/Sorting/SelectionSort.cs
@@ -0,0 +1,38 @@
+﻿using Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Algorithms.Searching_and_Sorting.Sorting
+{
+    public class SelectionSort : IExecutor
+    {
+        public void Execute()
+        {
+            List<int> list = new List<int>() { 1, 5, 6, 4, 6, 8, 5 };
+            PrintSortedList(list);
+        }
+
+        public void PrintSortedList(List<int> list)
+        {
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                int min_index = i;
+                for (int j = i + 1; j < list.Count; j++)
+                {
+                    if (list[j] < list[min_index])
+                        min_index = j;
+                }
+
+                int temp = list[min_index];
+                list[min_index] = list[i];
+                list[i] = temp;
+            }
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
