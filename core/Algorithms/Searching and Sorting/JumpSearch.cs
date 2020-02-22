19615f60ef984d1762d41535a7b777ba7cfddf36
aman takey <amantakey008@gmail.com>
2020-02-22 16:26:18 +0530

    jump search implementation

--- core/Algorithms/Searching and Sorting/JumpSearch.cs
+++ core/Algorithms/Searching and Sorting/JumpSearch.cs
@@ -10,12 +10,31 @@ namespace Core.Algorithms.Searching_and_Sorting
         public void Execute()
         {
             List<int> list = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-            Console.WriteLine(Search(list, 9, 0, list.Count));
+            Console.WriteLine(Search(list, 6));
         }
 
-        private bool Search(List<int> list, int v1, int v2, int count)
+        private int Search(List<int> list, int searchVal)
         {
-            throw new NotImplementedException();
+            int step = (int)Math.Floor(Math.Sqrt(list.Count));
+
+            int prev = 0;
+            while (list[Math.Min(step, list.Count - 1)] < searchVal)
+            {
+                prev = step;
+                step += (int)Math.Floor(Math.Sqrt(list.Count));
+                if (prev > list.Count)
+                    return -1;
+            }
+
+            while (list[prev] < searchVal)
+            {
+                prev++;
+                if (prev == Math.Min(step, list.Count))
+                    return -1;
+            }
+            if (list[prev] == searchVal)
+                return prev+1;
+            return -1;
         }
     }
 }
